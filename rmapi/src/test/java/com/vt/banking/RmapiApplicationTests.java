46405514a5cf923b4dac6c4dd6b5287488410152
eswaribala <parameswaribala@gmail.com>
2020-12-21 17:08:55 +0530

    rmapi v1

---
+++ rmapi/src/test/java/com/vt/banking/RmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vt.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
